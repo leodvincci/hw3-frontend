25f6df6bf9d464d2352a52805cbbd4009a082004
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 <lpenrose@depaul.edu>
2022-09-23 16:07:24 -0500

    changed ec2 link again

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const body = document.body;
 
-fetch('http://ec2-34-238-151-86.compute-1.amazonaws.com:8000/dbversion')
+fetch('34.238.151.86:8000/dbversion')
     .then((response) => response.json())
     .then((data) => body.append(data[0].versionString));
 
